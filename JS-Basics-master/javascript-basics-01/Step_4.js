78eddf3e4b3f35c80fb8140751a8d3e9b15abd04
rayan-aly <rayan.ali8204@gmail.com>
2021-03-06 20:43:12 +0200

    Completed Step_4 Ex

---
+++ JS-Basics-master/javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+function msg(){
+var name=document.getElementById('name').value;
+var surname=document.getElementById('surname').value;
+var city=document.getElementById('city').value;
+window.alert("Name : " +name+"\nPrénom : "+surname +"\nVille : "+city);
+}
